ba3406c6cc28d067dab83ce3811a7455b59d6d55
Abhishek Mukherjee <mukherjeeabhishek207@gmail.com>
2022-05-07 13:42:27 +0530

    completed the project

---
+++ app.js
@@ -0,0 +1,17 @@
+const closedFaced = document.querySelector('.closed');
+const openFaced = document.querySelector('.open');
+
+// Add event listener
+closedFaced.addEventListener('click', () => {
+    if (openFaced.classList.contains('open')){
+        openFaced.classList.add('active');
+        closedFaced.classList.remove('active');
+    }
+});
+
+openFaced.addEventListener('click', () => {
+    if(closedFaced.classList.contains('closed')){
+        closedFaced.classList.add('active');
+        openFaced.classList.remove('active');
+    }
+});
